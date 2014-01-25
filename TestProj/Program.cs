ab06f0a3b50113ffebbdf8293d3a3361829d8a15
komlosizsolt <komlosizsolt@freemail.hu>
2014-01-25 15:36:41 +0100

    Comment

--- TestProj/Program.cs
+++ TestProj/Program.cs
@@ -11,7 +11,7 @@ namespace TestProj
         static void Main(string[] args)
         {
             Console.WriteLine("Hello");
-            //comment
+            //comment ez van vag ynem
             Console.ReadKey();
         }
     }
